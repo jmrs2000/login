e03e49d5ce30ef77658a2fc3c8395e73a222f3d1
Juanma <jmramirez@itpfp.com>
2024-12-17 14:09:22 +0100

    Creacion de la contraseña y usuario

--- Formulario/Form1.cs
+++ Formulario/Form1.cs
@@ -12,13 +12,20 @@ namespace Formulario
 {
     public partial class Form1 : Form
     {
+        private string nombre = "paco";
+
+        private string contraseña = "contraseña";
+
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
+
+            
 
         }
 
@@ -29,6 +36,16 @@ namespace Formulario
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if ((textBox1.Text == nombre) && (textBox2.Text == nombre))
+            {
+                MessageBox.Show("Conexion establecida");
+
+            }
+            else
+            {
+                MessageBox.Show("No son iguales, error");
+
+            }
 
         }
     }
