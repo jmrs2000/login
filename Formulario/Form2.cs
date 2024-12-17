2fff8d33c578f93655a8d515a781142866fbb9e6
Juanma <jmramirez@itpfp.com>
2024-12-17 14:24:08 +0100

    Login hecho y boton de volver en el form 2

--- Formulario/Form2.cs
+++ Formulario/Form2.cs
@@ -16,5 +16,13 @@ namespace Formulario
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();
+
+            this.Close();
+        }
     }
 }
